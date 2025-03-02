fda9dfcbbd2373499772bcf83025133002539e49
Vinicius <comercial.viniciusgabriel@gmail.com>
2025-03-01 22:15:41 -0300

    feat: ItensService ok

--- src/main/java/com/example/serverless/serverless/service/ItensService.java
+++ src/main/java/com/example/serverless/serverless/service/ItensService.java
@@ -1,8 +1,39 @@
 package com.example.serverless.serverless.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+
+import com.example.serverless.serverless.entity.Itens;
+import com.example.serverless.serverless.repository.ItensRepository;
 
 @Service
 public class ItensService {
     
+    @Autowired
+    private ItensRepository itensRepository;
+
+    public List<Itens> findAll() {
+        return itensRepository.findAll();
+    }
+
+    public Itens post (Itens item) {
+        Assert.notNull(item.getNome(), "O nome é obrigatório");
+        Assert.notNull(item.getPreco(), "O preço é obrigatório");
+        Assert.notNull(item.getQuantidade(), "A quantidade é obrigatória");
+        return itensRepository.save(item);
+    }
+
+    public Itens put (Itens item) {
+        Assert.notNull(item.getNome(), "O nome é obrigatório");
+        Assert.notNull(item.getPreco(), "O preço é obrigatório");
+        Assert.notNull(item.getQuantidade(), "A quantidade é obrigatória");
+        return itensRepository.save(item);
+    }
+
+    public void delete(String nome) {
+        itensRepository.deleteById(nome);
+    }
 }
